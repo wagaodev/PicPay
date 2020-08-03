2a9ed077872f5c47fac3c2039c0fe7f787fa6961
wcfx <wcfx.dev@gmail.com>
2020-08-03 05:24:27 -0300

    Criacao do README

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
